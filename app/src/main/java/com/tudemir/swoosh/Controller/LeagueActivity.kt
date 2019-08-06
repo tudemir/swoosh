034f8c3dbc2d584feb99148136865f0ea98012d5
tudemir <tugbademir4@gmail.com>
2019-08-06 11:32:17 +0300

    App icon was created&loading state when rotation is done

--- app/src/main/java/com/tudemir/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/tudemir/swoosh/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.tudemir.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.tudemir.swoosh.Model.Player
@@ -12,10 +13,24 @@ import kotlinx.android.synthetic.main.activity_league.*
 class LeagueActivity : BaseActivity() {
 
     var player = Player("","")
+
+    //When we rotate the device, save player's current state
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState !=null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        }
+    }
+
 
     fun onMensClicked(view:View){
         womensLeagueBtn.isChecked = false
